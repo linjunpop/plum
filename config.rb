c9b7b921429f523a0319fb9a87085b6a95751ca8
Jun Lin <linjunpop@gmail.com>
2012-01-06 22:08:07 +0800

    config

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/plum/"
-http_images_path = "/plum/images"
-http_fonts_path = "/plum/fonts"
-css_dir = "public/plum/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
